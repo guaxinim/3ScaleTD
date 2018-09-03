b10187682c004c3a7635302c59003deac61fdbdc
Elvis Rocha <elvishrocha@gmail.com>
2018-09-03 14:14:18 -0300

    Update ProductsApplication.java

--- Products/src/main/java/com/redhat/service/ProductsApplication.java
+++ Products/src/main/java/com/redhat/service/ProductsApplication.java
@@ -5,7 +5,7 @@ import javax.ws.rs.core.Application;
 
 import io.swagger.jaxrs.config.BeanConfig;
 
-@ApplicationPath("rest")
+@ApplicationPath("issuerCard")
 public class ProductsApplication extends Application {
 	public ProductsApplication(){
 		 BeanConfig beanConfig = new BeanConfig();
